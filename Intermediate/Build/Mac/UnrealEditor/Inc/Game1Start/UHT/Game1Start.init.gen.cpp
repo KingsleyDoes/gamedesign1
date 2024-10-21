08e8c3647f82b43acfd2efcb64e7d492239a43be
Orion Kingsley <jmjnok@gmail.com>
2024-10-21 09:27:40 -0500

    built

---
+++ Intermediate/Build/Mac/UnrealEditor/Inc/Game1Start/UHT/Game1Start.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeGame1Start_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_Game1Start;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_Game1Start()
+	{
+		if (!Z_Registration_Info_UPackage__Script_Game1Start.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Game1Start",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xC995DEF2,
+				0x64C3833A,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Game1Start.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_Game1Start.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Game1Start(Z_Construct_UPackage__Script_Game1Start, TEXT("/Script/Game1Start"), Z_Registration_Info_UPackage__Script_Game1Start, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xC995DEF2, 0x64C3833A));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
